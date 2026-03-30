30f649782b5c3b7aa347602ae6c32bf024750d2a
gugudao <79732257+gugudao@users.noreply.github.com>
2026-03-31 04:57:36 +0800

    添加了对原始力量和添柴的Patch并同步添加了相应本地化内容；卡图未更换，因为还没有准备相应资源 (#5)
    * 添加了对原始力量和添柴的Patch并同步添加了相应本地化内容；卡图未更换，因为还没有准备相应资源
    
    * 后两个commit只是在反复横跳；第一次删除了Steam库和Godot引擎的硬编码  路径，导致我本地引用再次全部爆炸；另外注意到csproj已被加入到git.ignore中，第二次回滚了对csproj的改动

---
+++ SBModCode/CardsFix/PrimalForcePatch.cs
@@ -0,0 +1,59 @@
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
+
+
+namespace SBMod.SBModCode.CardsFix;
+
+[HarmonyPatch(typeof(PrimalForce))]
+public static class PrimalForcePatch
+{
+    [HarmonyPatch("ExtraHoverTips", MethodType.Getter)]
+    [HarmonyPostfix]
+    static void ExtraHoverTipsPostfix(PrimalForce __instance, ref IEnumerable<IHoverTip> __result)
+    {
+        __result = HoverTipFactory.FromCardWithCardHoverTips<Snakebite>();
+    }
+
+    [HarmonyPatch("OnPlay")]
+    [HarmonyPrefix]
+    static bool OnPlayPrefix(PrimalForce __instance, PlayerChoiceContext choiceContext, CardPlay cardPlay, ref Task __result)
+    {
+        __result = PatchOnPlay(__instance, choiceContext, cardPlay);
+        return false;
+    }
+    static async Task PatchOnPlay(PrimalForce instance, PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+
+        var owner = instance.Owner;
+        var state = instance.CombatState;
+
+        if (state == null || owner == null)
+            return;
+
+        await CreatureCmd.TriggerAnim(owner.Creature, "Cast", owner.Character.CastAnimDelay);
+
+        var hand = PileType.Hand.GetPile(owner).Cards
+            .Where(c => c != null && c.IsTransformable && c.Type == CardType.Skill)
+            .ToList();
+
+        if (hand.Count == 0)
+            return;
+
+        foreach (var original in hand)
+        {
+            CardModel newCard = state.CreateCard<Snakebite>(owner); 
+            if (instance.IsUpgraded && newCard != null)
+                CardCmd.Upgrade(newCard);
+
+            if (newCard != null)
+                await CardCmd.Transform(original, newCard);
+        }
+    }
+}
+
---
+++ SBModCode/CardsFix/StokePatch.cs
@@ -0,0 +1,64 @@
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Factories;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.Nodes.CommonUi;
+using SBMod.SBModCode.Extensions;
+
+
+namespace SBMod.SBModCode.CardsFix;
+
+[HarmonyPatch(typeof(Stoke))]
+public static class StokePatch
+{
+    [HarmonyPatch("OnPlay")]
+    [HarmonyPrefix]
+    static bool OnPlayPrefix(Stoke __instance, PlayerChoiceContext choiceContext, CardPlay cardPlay, ref Task __result)
+    {
+        __result = PatchOnPlay(__instance, choiceContext, cardPlay);
+        return false;
+    }
+    static async Task PatchOnPlay(Stoke instance, PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+
+        var owner = instance.Owner;
+        var state = instance.CombatState;
+
+        await CreatureCmd.TriggerAnim(owner.Creature, "Cast", owner.Character.CastAnimDelay);
+        List<CardModel> list = PileType.Hand.GetPile(owner).Cards.ToList();
+        int exhaustCount = list.Count;
+        foreach (CardModel item in list)
+        {
+            await CardCmd.Exhaust(choiceContext, item);
+        }
+
+        /*var allCards = owner.Character.CardPool
+    .GetUnlockedCards(owner.UnlockState, owner.RunState.CardMultiplayerConstraint);
+
+        var snakebitePool = allCards
+            .Where(c => SnakebiteHelper.IsSnakebiteCard(c))
+            .ToList();
+
+        var cards = CardFactory.GetForCombat(
+            owner,
+            snakebitePool,
+            exhaustCount,
+            owner.RunState.Rng.CombatCardGeneration
+        ).ToList();*/
+        for (int i = 0; i < list.Count; i++)
+        {
+            var card = state.CreateCard<Snakebite>(owner);
+            if (instance.IsUpgraded)
+            {
+                CardCmd.Upgrade(card, CardPreviewStyle.None);
+            }
+            await CardPileCmd.AddGeneratedCardsToCombat([card], PileType.Hand, addedByPlayer: true);
+        }
+    }
+}
+
