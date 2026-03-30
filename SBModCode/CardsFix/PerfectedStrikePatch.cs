51a1175abf53f0fbab323af3858796c153e57f61
KJSatori <1817232528@qq.com>
2026-03-30 21:01:39 +0800

    2045: 修正完美打击显示bug (#1)
    显示施加的中毒层数与实际施加层数不符

--- SBModCode/CardsFix/PerfectedStrikePatch.cs
+++ SBModCode/CardsFix/PerfectedStrikePatch.cs
@@ -5,12 +5,32 @@ using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Cards;
 using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.ValueProps;
 
 namespace SBMod.SBModCode.CardsFix;
 
 [HarmonyPatch(typeof(PerfectedStrike))]
 public static class PerfectedStrikePatch
 {
+    [HarmonyPatch("CanonicalVars", MethodType.Getter)]
+    [HarmonyPostfix]
+    static void CanonicalVarsPostfix(PerfectedStrike __instance, ref IEnumerable<DynamicVar> __result)
+    {
+        var vars = __result.ToList();
+        for (int i = 0; i < vars.Count; i++)
+        {
+            if (vars[i] is CalculatedDamageVar calcVar)
+            {
+                vars[i] = new CalculatedDamageVar(ValueProp.Move).WithMultiplier((CardModel card, Creature? _) => 
+                    card.Owner.PlayerCombatState.AllCards.Count((CardModel card) => card is Snakebite));
+            }
+        }
+        __result = vars;
+    }
+
     [HarmonyPatch("OnPlay")]
     [HarmonyPrefix]
     static bool OnPlayPrefix(PerfectedStrike __instance, PlayerChoiceContext choiceContext, CardPlay cardPlay, ref Task __result)
