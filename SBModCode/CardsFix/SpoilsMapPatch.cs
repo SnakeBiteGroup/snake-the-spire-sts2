6e44669ab7ea3de4bfc261656e7e889cd363b64a
bd65-crt <yuciy65@gmail.com>
2026-04-01 05:35:22 +0800

    灼热藏蛇图 (#19)
    * 2115小修一下命名空间问题
    
    * 更新easysnake范畴。更新power本地化。
    
    * 修复藏宝图
    
    * 修复紧咬。
    
    * 修复紧咬
    
    * 调整本地化
    
    * 做了灼热藏蛇图。还未测试，不知道是不是有问题。大家先测试一下
    
    ---------
    
    Co-authored-by: gugudao <79732257+gugudao@users.noreply.github.com>

--- SBModCode/CardsFix/SpoilsMapPatch.cs
+++ SBModCode/CardsFix/SpoilsMapPatch.cs
@@ -1,34 +1,60 @@
 using HarmonyLib;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Cards;
+using System;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Threading.Tasks;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Context;
+using MegaCrit.Sts2.Core.Nodes.CommonUi;
+using MegaCrit.Sts2.Core.TestSupport;
 
 namespace SBMod.SBModCode.CardsFix;
 
 [HarmonyPatch(typeof(SpoilsMap))]
 public static class SpoilsMapPatch
 {
+    [HarmonyPatch("MaxUpgradeLevel", MethodType.Getter)]
+    [HarmonyTranspiler]
+    static IEnumerable<CodeInstruction> MaxUpgradeLevelTranspiler(IEnumerable<CodeInstruction> instructions)
+    {
+        return new List<CodeInstruction>
+        {
+            new CodeInstruction(OpCodes.Ldc_I4, int.MaxValue),
+            new CodeInstruction(OpCodes.Ret)
+        }.AsEnumerable();
+    }
+
     [HarmonyPatch("OnQuestComplete")]
     [HarmonyPrefix]
     static bool OnQuestCompletePrefix(SpoilsMap __instance, ref Task<int> __result)
     {
-        __result = PatchOnQuestComplete(__instance);
-        return false;
-    }
-
-    static async Task<int> PatchOnQuestComplete(SpoilsMap instance)
-    {
+        int snakeCount = (int)__instance.DynamicVars.Gold.IntValue;
+        
         List<CardModel> cardsToAdd = new List<CardModel>();
-        for (int i = 0; i < 600; i++)
+        for (int i = 0; i < snakeCount; i++)
         {
-            cardsToAdd.Add(instance.Owner.RunState.CreateCard<Snakebite>(instance.Owner));
+            cardsToAdd.Add(__instance.Owner.RunState.CreateCard<Snakebite>(__instance.Owner));
         }
-        await CardPileCmd.Add(cardsToAdd, PileType.Deck);
-        PlayerCmd.CompleteQuest(instance);
-        await CardPileCmd.RemoveFromDeck(instance);
-        return 600;
+        
+        var results = CardPileCmd.Add(cardsToAdd, PileType.Deck).Result;
+        
+        if (!TestMode.IsOn && 
+            !CombatManager.Instance.IsEnding && 
+            LocalContext.IsMine(__instance))
+        {
+            CardCmd.PreviewCardPileAdd(results, time: 3f, CardPreviewStyle.MessyLayout);
+        }
+        
+        PlayerCmd.CompleteQuest(__instance);
+        CardPileCmd.RemoveFromDeck(__instance).Wait();
+        
+        __result = Task.FromResult(snakeCount);
+        return false;
     }
 }
