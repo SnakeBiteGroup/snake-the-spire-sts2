ccb8318e15a79d1485cd8010be13bfd7c5b58bed
bd65-crt <yuciy65@gmail.com>
2026-04-01 05:24:56 +0800

    做了灼热藏蛇图。还未测试，不知道是不是有问题。大家先测试一下

---
+++ SBModCode/Extensions/CardUpgradePatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
+
+namespace SBMod.SBModCode.Extensions;
+
+[HarmonyPatch(typeof(CardModel), "OnUpgrade")]
+public class CardUpgradePatch
+{
+    static void Postfix(CardModel __instance)
+    {
+        if (__instance is SpoilsMap)
+        {
+            int currentBase = (int)__instance.DynamicVars.Gold.BaseValue;
+            __instance.DynamicVars.Gold.UpgradeValueBy(currentBase);
+        }
+    }
+}
