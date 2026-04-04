858f1dfa6f5359e2c905966982ead23871657272
KJSatori <1817232528@qq.com>
2026-04-05 00:23:05 +0800

    0011:修复小咬的各种bug (#27)

--- SBModCode/CardsFix/Token/ShivPatch.cs
+++ SBModCode/CardsFix/Token/ShivPatch.cs
@@ -6,20 +6,34 @@ using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models.Cards;
 using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.Commands.Builders;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Nodes.Vfx;
+using Godot;
 
 namespace SBMod.SBModCode.CardsFix.Token;
 
 [HarmonyPatch(typeof(Shiv))]
 public static class ShivPatch
 {
-    [HarmonyPatch(MethodType.Constructor)]
+    [HarmonyPatch("CanonicalVars", MethodType.Getter)]
     [HarmonyPostfix]
-    static void ConstructorPostfix(Shiv __instance)
+    static void CanonicalVarsPostfix(Shiv __instance, ref IEnumerable<DynamicVar> __result)
     {
-        var typeField = AccessTools.Field(typeof(CardModel), "<Type>k__BackingField");
-        typeField?.SetValue(__instance, CardType.Skill);
+        var newList = new List<DynamicVar>
+        {
+            new DamageVar(3m, ValueProp.Move),
+            new PowerVar<PoisonPower>(1m),
+		    new CalculationBaseVar(0m),
+		    new CalculationExtraVar(1m),
+		    new CalculatedVar("FanOfKnivesAmount").WithMultiplier((CardModel card, Creature? _) => 
+                (card != null && card.IsMutable && card.Owner != null) ? card.Owner.Creature.GetPowerAmount<FanOfKnivesPower>() : 0)
+        };
+        __result = newList;
     }
-    
+
     [HarmonyPatch("OnPlay")]
     [HarmonyPrefix]
     static bool OnPlayPrefix(Shiv __instance, PlayerChoiceContext choiceContext, CardPlay cardPlay, ref Task __result)
@@ -31,33 +45,41 @@ public static class ShivPatch
     static async Task PatchOnPlay(Shiv instance, PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
         var hasFanOfKnives = false;
+        AttackCommand attackCommand = DamageCmd.Attack(instance.DynamicVars.Damage.BaseValue).FromCard(instance);
         if (CombatManager.Instance.IsInProgress && instance.Owner != null)
         {
             hasFanOfKnives = instance.Owner.Creature.HasPower<FanOfKnivesPower>();
         }
 
-        var totalPoison = instance.DynamicVars.Damage.BaseValue;
-
         if (hasFanOfKnives)
         {
             var hittableEnemies = instance.CombatState.HittableEnemies;
+            Creature lastEnemy = hittableEnemies.LastOrDefault();
+            attackCommand = attackCommand.TargetingAllOpponents(instance.CombatState).WithHitVfxNode(_ => NShivThrowVfx.Create(instance.Owner.Creature, lastEnemy, Colors.Green));
             foreach (var enemy in hittableEnemies)
             {
-                await PowerCmd.Apply<PoisonPower>(enemy, totalPoison, instance.Owner.Creature, instance);
+                await PowerCmd.Apply<PoisonPower>(enemy, instance.DynamicVars.Poison.BaseValue, instance.Owner.Creature, instance);
             }
         }
         else
         {
             ArgumentNullException.ThrowIfNull(cardPlay.Target, "cardPlay.Target");
-            await PowerCmd.Apply<PoisonPower>(cardPlay.Target, totalPoison, instance.Owner.Creature, instance);
+            attackCommand = attackCommand.Targeting(cardPlay.Target).WithHitVfxNode((Creature t) => NShivThrowVfx.Create(instance.Owner.Creature, t, Colors.Green));
+            await PowerCmd.Apply<PoisonPower>(cardPlay.Target, instance.DynamicVars.Poison.BaseValue, instance.Owner.Creature, instance);
         }
+        if (instance.Owner.Character is MegaCrit.Sts2.Core.Models.Characters.Silent)
+        {
+            attackCommand.WithAttackerAnim("Shiv", 0.2f);
+        }
+        await attackCommand.Execute(choiceContext);
     }
 
     [HarmonyPatch("OnUpgrade")]
     [HarmonyPrefix]
     static bool OnUpgradePrefix(Shiv __instance)
     {
-        __instance.DynamicVars.Damage.UpgradeValueBy(2m);
+        __instance.DynamicVars.Damage.UpgradeValueBy(1m);
+        __instance.DynamicVars.Poison.UpgradeValueBy(1m);
         return false;
     }
 }
